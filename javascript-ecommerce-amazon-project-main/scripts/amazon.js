a97ab1f5c4d1a9bdfea9c1caab813b843d70113a
Roel Somido <roel.somido@icloud.com>
2024-10-20 20:44:54 +0800

    add message when adding product to cart

--- javascript-ecommerce-amazon-project-main/scripts/amazon.js
+++ javascript-ecommerce-amazon-project-main/scripts/amazon.js
@@ -1,5 +1,7 @@
 let productsHMTL = '';
 
+const addedMessageTimeouts = {};
+
 products.forEach((product) => {
   productsHMTL += `
     <div class="product-container">
@@ -41,7 +43,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -94,5 +96,24 @@ document.querySelectorAll('.js-add-to-cart')
 
     document.querySelector('.js-cart-quantity')
       .innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+    const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000);
+  
+    addedMessageTimeouts[productId] = timeoutId
+
     });
   });
