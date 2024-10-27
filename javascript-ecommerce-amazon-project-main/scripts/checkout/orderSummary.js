d53f06e9e928a6f2279a2bc95f6ef8345d0df96e
Roel Somido <roel.somido@icloud.com>
2024-10-27 14:22:19 +0800

    Create automated tests for orderSummary

--- javascript-ecommerce-amazon-project-main/scripts/checkout/orderSummary.js
+++ javascript-ecommerce-amazon-project-main/scripts/checkout/orderSummary.js
@@ -34,7 +34,7 @@ const dateString = calculateDeliveryDate(deliveryOption);
 
 
 cartSummaryHTML +=`
-<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+<div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
   <div class="delivery-date">
     Delivery date: ${dateString};
   </div>
@@ -50,7 +50,7 @@ cartSummaryHTML +=`
       <div class="product-price">
         $${formatCurrency(matchingProduct.priceCents)}
       </div>
-      <div class="product-quantity">
+      <div class="product-quantity js-product-quantity-${matchingProduct.id}">
         <span>
           Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
         </span>
@@ -61,7 +61,7 @@ cartSummaryHTML +=`
         <input class="quantity-input js-quantity-input-${matchingProduct.id}">
         <span class="save-quantity-link link-primary js-save-link"
         data-product-id="${matchingProduct.id}">Save</span>
-        <span class="delete-quantity-link link-primary js-delete-link"
+        <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}"
         data-product-id="${matchingProduct.id}">
           Delete
         </span>
