c99aa82768ebff36908c38c881ab167e381de848
Roel Somido <roel.somido@icloud.com>
2024-10-27 19:20:18 +0800

    Add more tests to the orderSummayTest, cartTest, and moneyTest

--- tests/checkout/orderSummaryTest.js
+++ tests/checkout/orderSummaryTest.js
@@ -43,6 +43,10 @@ describe('test suite: renderOrderSummary', () => {
       length).toEqual(2);
       expect(document.querySelector(`.js-product-quantity-${productId1}`).innerText).toContain('Quantity: 2');
       expect(document.querySelector(`.js-product-quantity-${productId2}`).innerText).toContain('Quantity: 1'); 
+      expect(document.querySelector(`.js-product-name-${productId1}`).innerText).toContain('Black and Gray Athletic Cotton Socks - 6 Pairs');
+      expect(document.querySelector(`.js-product-name-${productId2}`).innerText).toContain('Intermediate Size Basketball');
+      expect(document.querySelector(`.js-product-price-${productId1}`).innerText).toEqual(`$10.90`)
+      expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual(`$20.95`)
   
   });
 
@@ -55,6 +59,7 @@ describe('test suite: renderOrderSummary', () => {
       expect(document.querySelector(`.js-cart-item-container-${productId2}`)).not.toEqual(null);
       expect(cart.length).toEqual(1);
       expect(cart[0].productId).toEqual(productId2);
-      
+      expect(document.querySelector(`.js-product-name-${productId2}`).innerText).toContain('Intermediate Size Basketball');
+      expect(document.querySelector(`.js-product-price-${productId2}`).innerText).toEqual(`$20.95`)
   });
 });
