534cdf07f1a53f02abe3fe1bd05e3fcec8c71fb9
Roel Somido <roel.somido@icloud.com>
2024-11-04 11:42:47 +0800

    Use backend to load products

--- javascript-ecommerce-amazon-project-main/data/cart-oop.js
+++ javascript-ecommerce-amazon-project-main/data/cart-oop.js
@@ -105,11 +105,8 @@ class Cart {
 
 
 export const cart = new Cart('cart-oop');
-const businessCart = new Cart('cart-business');
+// const businessCart = new Cart('cart-business');
 
-console.log(cart);
-console.log(businessCart);
-console.log(businessCart instanceof Cart)
 
 
 
--- javascript-ecommerce-amazon-project-main/scripts/checkout.js
+++ javascript-ecommerce-amazon-project-main/scripts/checkout.js
@@ -1,8 +1,23 @@
 import {renderOrderSummary} from   './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
+import {loadCartFetch } from '../data/cart.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
 import '../data/cart-oop.js';
 
-renderOrderSummary();
-renderPaymentSummary();
-renderCheckoutHeader();
+async function loadPage() {
+  try {
+    await Promise.all([
+      loadProductsFetch(),
+      loadCartFetch()
+    ])
+
+  } catch(error) {
+    console.log('Unexpected error. Please try again later.');
+  }
+  renderOrderSummary();
+  renderPaymentSummary();
+  renderCheckoutHeader();
+}
+
+loadPage();
