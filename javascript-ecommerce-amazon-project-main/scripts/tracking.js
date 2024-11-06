56363e1f96aba46f57df3445ea1aa18506e0bbb9
Roel Somido <roel.somido@icloud.com>
2024-11-06 13:18:18 +0800

    Make tracking page interactive

--- javascript-ecommerce-amazon-project-main/scripts/tracking.js
+++ javascript-ecommerce-amazon-project-main/scripts/tracking.js
@@ -2,6 +2,7 @@ import { getOrder } from "../data/orders.js";
 import { getProduct, loadProductsFetch } from "../data/products.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
+
 async function loadPage() {
   await loadProductsFetch();
 
@@ -19,6 +20,11 @@ async function loadPage() {
       productDetails = details;
     }
   });
+
+  const today = dayjs();
+  const orderTime = dayjs(order.orderTime);
+  const deliveryTime = dayjs(productDetails.estimatedDeliveryTime);
+  const percentProgress = ((today - orderTime) / (deliveryTime - orderTime)) * 100;
 
   const trackingHTML = `
   <a class="back-to-orders-link link-primary" href="orders.html">
@@ -40,19 +46,19 @@ async function loadPage() {
     <img class="product-image" src="${product.image}">
 
     <div class="progress-labels-container">
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress < 100 ? "current status" : ''}">
         Preparing
       </div>
-      <div class="progress-label current-status">
+      <div class="progress-label ${(percentProgress >= 50 && percentProgress< 100) ? "current status" : ''}">
         Shipped
       </div>
-      <div class="progress-label">
+      <div class="progress-label ${percentProgress >= 100 ? "current status": ''}">
         Delivered
       </div>
     </div>
 
     <div class="progress-bar-container">
-      <div class="progress-bar"></div>
+      <div class="progress-bar" style="width: ${percentProgress}%;"></div>
     </div>
   `;
 
